0fa70e6800900b30ff79aef582d31356d68f1464
Gabriel Vieira <gabrielvieira1@protonmail.com>
2024-01-30 23:08:22 -0300

    Project created

---
+++ ViewModels/Command.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModels
+{
+  public class Command : ICommand
+  {
+    public event EventHandler CanExecuteChanged;
+
+    private void RaiseCanExecuteChanged()
+    {
+      if (CanExecuteChanged != null)
+        CanExecuteChanged(this, new EventArgs());
+    }
+
+    bool _CanExecute = true;
+
+    public bool CanExecute(object parameter)
+    {
+      return _CanExecute;
+    }
+
+    public async void Execute(object parameter)
+    {
+      _CanExecute = false;
+      RaiseCanExecuteChanged();
+
+      await _FT();
+
+      _CanExecute = true;
+      RaiseCanExecuteChanged();
+    }
+
+    public Command(Func<Task> Func)
+    {
+      _FT = Func;
+    }
+
+    Func<Task> _FT;
+  }
+}
